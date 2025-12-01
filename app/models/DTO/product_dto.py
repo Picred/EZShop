e26441414dd25c7b02d107d102aac11a0c18b902
Andrei Daniel Stefan <andrei.stefand@outlook.com>
2025-12-01 18:29:53 +0100

    feat: GET/POST /products + exceptions implemented

--- app/models/DTO/product_dto.py
+++ app/models/DTO/product_dto.py
@@ -4,9 +4,9 @@ from typing import Optional
 
 class ProductTypeDTO(BaseModel):
     id: Optional[int] = None
-    description: str = Field(min_length=1)
-    productCode: str = Field(min_length=1)
-    pricePerUnit: float
+    description: str = None
+    productCode: str = None
+    pricePerUnit: float = None
     note: str = Field(min_length=1)
     quantity: int
-    position: str = None
+    position: str
